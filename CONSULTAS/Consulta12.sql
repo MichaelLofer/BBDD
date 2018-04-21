b8a9b6f2bdc4203797a8c7db62ff0a2e2c84c0ac
Juan Casado Ballesteros <juan.casado@edu.uah.es>
2018-04-21 19:21:16 +0200

    CAMBIO DESCRIPCION SURTIDORES

---
+++ CONSULTAS/Consulta12.sql
@@ -0,0 +1,8 @@
+﻿(SELECT AVG("Precio") AS "PRECIO MEDIO", AVG("Capacidad") AS "CAPACIDAD MEDIA"
+	FROM "Surtidor"
+	INNER JOIN "Gasolina" ON "Surtidor"."Numero_surtidor" = "Gasolina"."Numero_surtidor")
+UNION
+(SELECT AVG("Precio") AS "PRECIO MEDIO", AVG("Capacidad") AS "CAPACIDAD MEDIA"
+	FROM "Surtidor"
+	INNER JOIN "Gasoleo" ON "Surtidor"."Numero_surtidor" = "Gasoleo"."Numero_surtidor")	
+	
