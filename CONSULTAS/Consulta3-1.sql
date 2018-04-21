c541a7cfb83b007380e592b5f8f6755036ad69f5
Juan Casado Ballesteros <juan.casado@edu.uah.es>
2018-04-21 18:40:31 +0200

    EXCEL

---
+++ CONSULTAS/Consulta3-1.sql
@@ -0,0 +1,4 @@
+﻿SELECT "Tipo", sum ("PvP"*"cantidad") as "Dinero"
+	FROM "Contiene"
+	INNER JOIN "Articulo" ON "Contiene"."Codigo_Barras" = "Articulo"."Codigo_Barras"
+	GROUP BY "Tipo"
---
+++ CONSULTAS/Consulta5.sql
@@ -0,0 +1,4 @@
+﻿SELECT "Nombre_Usuario", SUM ("Puntos_canjeados")as "Puntos Totales"
+	FROM "Canjea"
+	GROUP BY "Nombre_Usuario"
+	
