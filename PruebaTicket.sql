f622f7cace14cfd56d82220c452acff76ed3c049
Miguel Ángel Losada <miguel.losada@edu.uah.es>
2018-04-29 13:12:52 +0200

    prueba ticket

---
+++ PruebaTicket.sql
@@ -0,0 +1,5 @@
+﻿--UPDATE "Ticket" SET "Fecha_emision"='2010-08-27 22:16:46'
+--WHERE "Ticket"."Fecha_emision"='2010-08-28 22:16:46'
+--SELECT  "Codigo", "cantidad", "Articulo"."Precio_sin_IVA"
+--FROM "Contiene" INNER JOIN "Articulo" ON "Contiene"."Codigo_Barras"="Articulo"."Codigo_Barras"
+--GROUP BY "Codigo", "cantidad", "Articulo"."Precio_sin_IVA"
